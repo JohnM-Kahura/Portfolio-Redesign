dc9b46702ed8ca706ff9761ec2a15b55617ad360
JohnM-Kahura <Johnmungai392@gmail.com>
2022-08-19 16:01:52 +0300

    clean up

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -1,39 +1,39 @@
 // --------------------------------------- Project List
 export const ProjectList = [
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_1.png",
-    title: "Spense",
-    description: "Codewell challenge, Spense is a landing page for writers.",
-    tech_stack: "Html, Css, Javascript",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link",
+    img: "https://raw.githubusercontent.com/JohnM-Kahura/Kenyan-Eye-Blog/master/kenyan-eye.png",
+    title: "Kenyan Eye",
+    description: "Kenyan Eye is a blog where people can share their views on the political situation in my country",
+    tech_stack: "Python,Django,HTML,CSS,Tailwind,JavaScript,SQLite",
+    github_url: "https://github.com/JohnM-Kahura/Kenyan-Eye-Blog",
+    demo_url: "http://kenyan-eye.herokuapp.com",
   },
   {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_2.png",
-    title: "Fiber",
+    img: "https://github.com/JohnM-Kahura/Biometrics-SPM/blob/master/alter-tech-demo.png?raw=true",
+    title: "BioMetrics App",
     description:
-      "An online portfolio generator. Great to practice flex/grid layouts, absolute positioning and sliders.",
-    tech_stack: "Html, Css, Javascript",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link",
+      "This a desktop and Web app that is used to enroll students to ZKTeco biometrics Device.",
+    tech_stack: "Django, Django Rest Framework, Flutter,Postgres",
+    github_url: "https://github.com/JohnM-Kahura/Biometrics-SPM",
+    demo_url: "https://johnm-kahura.github.io",
   },
-  {
-    img: "https://github.com/JohnM-Kahura/Biometrics-SPM/blob/master/preview.png?raw=true",
-    title: "Portfolio",
-    description: "Ofcourse i made my own personal portfolio website.",
-    tech_stack: "Reactjs, Css",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link/",
-  },
-  {
-    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
-    title: "Ecommerce website",
-    description:
-      "Created simple ecommerce website using reactjs and react-use-cart plugin.",
-    tech_stack: "Reactjs, Css",
-    github_url: "https://your_project_github_link",
-    demo_url: "https://your_project_demo_link",
-  },
+  // {
+  //   img: "https://github.com/JohnM-Kahura/Biometrics-SPM/blob/master/preview.png?raw=true",
+  //   title: "Portfolio",
+  //   description: "Ofcourse i made my own personal portfolio website.",
+  //   tech_stack: "Reactjs, Css",
+  //   github_url: "https://your_project_github_link",
+  //   demo_url: "https://your_project_demo_link/",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
+  //   title: "Ecommerce website",
+  //   description:
+  //     "Created simple ecommerce website using reactjs and react-use-cart plugin.",
+  //   tech_stack: "Reactjs, Css",
+  //   github_url: "https://your_project_github_link",
+  //   demo_url: "https://your_project_demo_link",
+  // },
 ];
 
 // --------------------------------------- Skills
