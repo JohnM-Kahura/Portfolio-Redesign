d2388da4aeda2e67dfdafb7186079d000d8e371d
JohnM-Kahura <Johnmungai392@gmail.com>
2022-11-02 23:36:34 +0300

    added a fixed height to the project cards

--- src/components/Projects/ProjectCard/ProjectCard.js
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -13,7 +13,7 @@ function ProjectCard() {
       {ProjectList.map((list, index) => (
         <Card key={index}>
           <CardLeft>
-            <img src={list.img} alt={list.name} />
+            <img src={list.img} alt={list.name} style={{height:'300px'  }}/>
           </CardLeft>
           <CardRight>
             <h4>{list.title}</h4>
